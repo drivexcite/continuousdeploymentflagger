e5dedfd5294a3e79fd22666f7a906fecdb64cc0d
Tona Diaz <tdiaz@healthwise.org>
2020-12-03 19:18:41 -0700

    Significant changes (Still doesn't work, probably the conflict with ad-pod-identity 1.70 and kubenet)

--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -14,14 +14,14 @@ az login
 # Retrieve resource id from the Platform Container Registry
 $acrResourceId = az acr show --name $acrName --resource-group $acrResourceGroup --subscription $acrSubscription --query id
 
+# Retrieve the tenant Id from the KeyVault Subscription
+$globalTenantId = az account show --query "tenantId"
+
 # Create resource group
 az group create --name $resourceGroup --location westus
 
-# Create a service principal
-$principalObject = az ad sp create-for-rbac --skip-assignment | ConvertFrom-Json
-
 # Create cluster
-az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size Standard_B2s --generate-ssh-keys --node-count 3 --enable-managed-identity --attach-acr $acrResourceId --service-principal $principalObject.appId --client-secret $principalObject.password
+az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size Standard_B2s --generate-ssh-keys --node-count 3 --enable-managed-identity --attach-acr $acrResourceId
 
 # Create local configuration file to talk to the AKS Cluster
 az aks get-credentials --resource-group $resourceGroup --name $clusterName
@@ -29,48 +29,44 @@ az aks get-credentials --resource-group $resourceGroup --name $clusterName
 # Add helm chart for KeyVault to Kubernetes Secrets driver
 # choco upgrade  kubernetes-helm
 helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts
-helm install csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --generate-name
 
-# Collect principals and resource group for Managed Identity Role Assignments
-$aksClusterInfo = az aks show --resource-group $resourceGroup --name $clusterName | ConvertFrom-Json
-
-$aksManagedIdentityClientId = $aksClusterInfo.identityProfile.kubeletidentity.clientId
-$aksClusterNodeResourceGroup = $aksClusterInfo.nodeResourceGroup
-
-# Assign roles to the AKS Managed Identity
-az role assignment create --role "Managed Identity Operator"   --assignee $aksManagedIdentityClientId --scope /subscriptions/$mainSubscription/resourcegroups/$resourceGroup
-az role assignment create --role "Managed Identity Operator"   --assignee $aksManagedIdentityClientId --scope /subscriptions/$mainSubscription/resourcegroups/$aksClusterNodeResourceGroup
-az role assignment create --role "Virtual Machine Contributor" --assignee $aksManagedIdentityClientId --scope /subscriptions/$mainSubscription/resourcegroups/$aksClusterNodeResourceGroup
-
-# Create an AD Identity
-$identityName = 'user-assigned-identity'
-$identityInfo = az identity create -g $resourceGroup -n $identityName | ConvertFrom-Json
-
-$managedIdentityClientId = $identityInfo.clientId
-$managedIdentityPrincipalId = $identityInfo.principalId
-
-# Assigned the identity as a Reader in KeyVault
-az role assignment create --role "Reader" --assignee $managedIdentityPrincipalId --scope /subscriptions/$acrSubscription/resourceGroups/$acrResourceGroup/providers/Microsoft.KeyVault/vaults/$keyVaultName
-az role assignment create --role "Reader" --assignee $managedIdentityClientId --scope /subscriptions/$acrSubscription/resourceGroups/$acrResourceGroup/providers/Microsoft.KeyVault/vaults/$keyVaultName
-az role assignment create --role "Reader" --assignee $aksManagedIdentityClientId --scope /subscriptions/$acrSubscription/resourceGroups/$acrResourceGroup/providers/Microsoft.KeyVault/vaults/$keyVaultName
-az role assignment create --role "Reader" --assignee $principalObject.appId --scope /subscriptions/$acrSubscription/resourceGroups/$acrResourceGroup/providers/Microsoft.KeyVault/vaults/$keyVaultName
-
-az keyvault set-policy --name $keyVaultName --resource-group $acrResourceGroup --subscription $acrSubscription --secret-permissions get --spn $managedIdentityClientId
-az keyvault set-policy --name $keyVaultName --resource-group $acrResourceGroup --subscription $acrSubscription --key-permissions get --spn $managedIdentityClientId
-
-az keyvault set-policy --name $keyVaultName --resource-group $acrResourceGroup --subscription $acrSubscription --key-permissions get --spn $aksManagedIdentityClientId
-az keyvault set-policy --name $keyVaultName --resource-group $acrResourceGroup --subscription $acrSubscription --key-permissions get --spn $aksManagedIdentityClientId
-
-# Install AD Identity to AKS
-helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
-helm install pod-identity aad-pod-identity/aad-pod-identity
-
-# Retrieve the tenant Id from the KeyVault Subscription
-$globalTenantId = az account show --query "tenantId"
+kubectl create ns csi-driver
+helm install csi-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace csi-driver
 
 # Expand and Apply the Secret Provider Class to the cluster
 Get-Content .\SecretProviderClass.yaml.template | foreach { $ExecutionContext.InvokeCommand.ExpandString($_) } | Set-Content .\SecretProviderClass.yaml
 kubectl apply -f .\SecretProviderClass.yaml
+
+# Collect principals and resource group for Managed Identity Role Assignments
+$aks = az aks show --resource-group $resourceGroup --name $clusterName | ConvertFrom-Json
+
+# Create role assignments for AKS Managed Identity
+az role assignment create --role "Managed Identity Operator" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$($aks.nodeResourceGroup)
+az role assignment create --role "Virtual Machine Contributor" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$($aks.nodeResourceGroup)
+az role assignment create --role "Managed Identity Operator" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$resourceGroup
+
+# Install AD Identity to AKS
+helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
+
+kubectl create ns ad-identity
+helm install aad-pod-identity aad-pod-identity/aad-pod-identity --namesp ace ad-identity --set nmi.allowNetworkPluginKubenet=true
+
+# Retrieve Cluster's Managed Identity
+$userAssignedIdentity = az resource list -g $aks.nodeResourceGroup --query "[?contains(type, 'Microsoft.ManagedIdentity/userAssignedIdentities')]"  | ConvertFrom-Json
+$identity = az identity show -n $userAssignedIdentity.name -g $userAssignedIdentity.resourceGroup | ConvertFrom-Json
+
+$identityName = $identity.name.ToLower()
+$identityId = $identity.id
+$identityClientId = $identity.clientId
+
+# Retrieve KeyVault info
+$keyVault = az keyvault show -n $keyVaultName  -g $acrResourceGroup --subscription $acrSubscription | ConvertFrom-Json
+
+# Assign reader role for KeyVault to Cluster's Managed Identity
+az role assignment create --role "Reader" --assignee $identity.principalId --scope $keyVault.id
+
+# Set Secret Policys for Identity
+az keyvault set-policy -n $keyVaultName -g $acrResourceGroup --subscription $acrSubscription --secret-permissions get --spn $identity.clientId
 
 # Expand and Apply the Identity and Binding
 Get-Content .\PodIdentityAndBinding.yaml.template | foreach { $ExecutionContext.InvokeCommand.ExpandString($_) } | Set-Content .\PodIdentityAndBinding.yaml
