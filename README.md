c765ca438774f6c09071f71a4c218e37760513a4
Tona Diaz <tdiaz@healthwise.org>
2020-12-07 12:54:30 -0700

    Simplified infrastructure scripts, propagated secrets for environment variables in the pod.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # continuousdeploymentflagger
+
+# Relevant Links (Errors found during configuration of the AD Pod Identity Provider)
+[Secure secrets with Key Vault](https://github.com/HoussemDellai/aks-keyvault)
+[Securing Secrets in AKS using Key Vault](https://www.youtube.com/watch?v=dAFWrbeA6vQ&list=PLpbcUe4chE79sB7Jg7B4z3HytqUUEwcNE&index=24)
+[404 getting assigned identities for pod](https://github.com/Azure/secrets-store-csi-driver-provider-azure/issues/119)
+[Help needed configuring aad-pod-identity](https://github.com/Azure/aad-pod-identity/issues/414)
+[mic pod has insufficient permissions on AKS](https://github.com/Azure/aad-pod-identity/issues/38)
+[MIC compute.VirtualMachineScaleSetsClient#CreateOrUpdate LinkedAuthorizationFailed when AKS node-pool uses a subnet from another resource group](https://github.com/Azure/aad-pod-identity/issues/511)
+[Mounting volumes and exporting environment variables for container](https://github.com/Azure/secrets-store-csi-driver-provider-azure/issues/133)
--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -1,8 +1,8 @@
 
 # Location of the ACR and AKV is for this example in a separate subscription
 $mainSubscription = '45d3970c-1cf3-47f8-86fa-ae717be4baa9'
-$resourceGroup = 'MyDeploymentGroup'
-$clusterName = 'MyContiniousCluster'
+$resourceGroup = 'ContinuousDeploymentGroup'
+$clusterName = 'ContiniousCluster'
 
 $acrName = 'hwpacrwestus'
 $acrResourceGroup = 'hwp-rg-westus-platform'
@@ -12,6 +12,7 @@ $keyVaultName = 'hwp-keyvault-s-westus'
 
 # Login to Azure
 az login
+az account set --subscription $mainSubscription
 
 # Retrieve resource id from the Platform Container Registry
 $acrResourceId = az acr show --name $acrName --resource-group $acrResourceGroup --subscription $acrSubscription --query id
@@ -28,26 +29,8 @@ az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size
 # Create local configuration file to talk to the AKS Cluster
 az aks get-credentials --resource-group $resourceGroup --name $clusterName
 
-# Add helm chart for KeyVault to Kubernetes Secrets driver
-# choco upgrade  kubernetes-helm
-helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts
-
-kubectl create ns csi-driver
-helm install csi-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace csi-driver
-
 # Collect principals and resource group for Managed Identity Role Assignments
 $aks = az aks show --resource-group $resourceGroup --name $clusterName | ConvertFrom-Json
-
-# Create role assignments for AKS Managed Identity
-az role assignment create --role "Managed Identity Operator" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$($aks.nodeResourceGroup)
-az role assignment create --role "Virtual Machine Contributor" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$($aks.nodeResourceGroup)
-az role assignment create --role "Managed Identity Operator" --assignee $aks.identityProfile.kubeletidentity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$resourceGroup
-
-# Install AD Identity to AKS
-helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
-
-kubectl create ns ad-identity
-helm install aad-pod-identity aad-pod-identity/aad-pod-identity --namespace ad-identity --set nmi.allowNetworkPluginKubenet=true
 
 # Retrieve Cluster's Managed Identity
 $userAssignedIdentity = az resource list -g $aks.nodeResourceGroup --query "[?contains(type, 'Microsoft.ManagedIdentity/userAssignedIdentities')]"  | ConvertFrom-Json
@@ -61,10 +44,6 @@ az role assignment create --role "Managed Identity Operator" --assignee $identit
 az role assignment create --role "Virtual Machine Contributor" --assignee $identity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$($aks.nodeResourceGroup)
 az role assignment create --role "Managed Identity Operator" --assignee $identity.clientId --scope /subscriptions/$mainSubscription/resourcegroups/$resourceGroup
 
-# These two will fail (may not be necessary)
-# az role assignment create --role "Virtual Machine Contributor" --assignee $identity.clientId --scope /subscriptions/$acrSubscription/resourcegroups/acrResourceGroup
-# az role assignment create --role "Managed Identity Operator" --assignee $identity.clientId --scope /subscriptions/$acrSubscription/resourcegroups/$acrResourceGroup
-
 # Retrieve KeyVault info
 $keyVault = az keyvault show -n $keyVaultName  -g $acrResourceGroup --subscription $acrSubscription | ConvertFrom-Json
 
@@ -73,6 +52,19 @@ az role assignment create --role "Reader" --assignee $identity.principalId --sco
 
 # Set Secret Policys for Identity
 az keyvault set-policy -n $keyVaultName -g $acrResourceGroup --subscription $acrSubscription --secret-permissions get --spn $identity.clientId
+
+# Add helm chart for KeyVault to Kubernetes Secrets driver
+# choco upgrade  kubernetes-helm
+helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts
+
+kubectl create ns csi-driver
+helm install csi-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace csi-driver
+
+# Install AD Identity to AKS
+helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
+
+kubectl create ns ad-identity
+helm install aad-pod-identity aad-pod-identity/aad-pod-identity --namespace ad-identity --set nmi.allowNetworkPluginKubenet=true
 
 # Expand and Apply the Secret Provider Class to the cluster
 Get-Content .\SecretProviderClass.yaml.template | foreach { $ExecutionContext.InvokeCommand.ExpandString($_) } | Set-Content .\SecretProviderClass.yaml
@@ -84,8 +76,12 @@ kubectl apply -f .\PodIdentityAndBinding.yaml
 
 # Install any pod with secrets mounted
 kubectl apply -f .\pod.yaml
-kubectl describe pod/nginx-secrets-store-inline
+kubectl describe pod/nginx-secrets-store
 
 # Check the pod to see if it started and if the secrets are mounted
 kubectl describe pod/nginx-secrets-store
 kubectl exec -it nginx-secrets-store ls /mnt/secrets-store/
+
+# Install Istio
+# choco install istioctl
+istioctl install --set profile=demo -y
